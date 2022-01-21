f93f292f095ceeb8cad546fe92aba16da4f52a70
alsami <al.khatib.sami1990@gmail.com>
2022-01-21 08:40:54 +0100

    feat: add test to validate that host-proxy works with a web-host test-server

--- test/alsami.Duende.IdentityServer.AspNetCore.Testing.Tests/IdentityServerHostProxyTests.cs
+++ test/alsami.Duende.IdentityServer.AspNetCore.Testing.Tests/IdentityServerHostProxyTests.cs
@@ -3,7 +3,13 @@ using alsami.Duende.IdentityServer.AspNetCore.Testing.Builder;
 using alsami.Duende.IdentityServer.AspNetCore.Testing.Configuration;
 using alsami.Duende.IdentityServer.AspNetCore.Testing.Services;
 using alsami.Duende.IdentityServer.AspNetCore.Testing.Tests.Shared;
+using Duende.IdentityServer.Api;
 using Duende.IdentityServer.Models;
+using IdentityModel.Client;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace alsami.Duende.IdentityServer.AspNetCore.Testing.Tests
@@ -39,6 +45,50 @@ namespace alsami.Duende.IdentityServer.AspNetCore.Testing.Tests
             var response = await identityServerProxy.GetDiscoverResponseAsync();
 
             Assert.NotNull(response);
+        }
+        
+        [Fact]
+        public async Task IdentityServerProxy_GetClientCredentialsAsync_Authorize_Api_Succeeds()
+        {
+            var clientConfiguration = new ClientConfiguration("MyClient", "MySecret");
+
+            var client = new Client
+            {
+                ClientId = clientConfiguration.Id,
+                ClientSecrets = new List<Secret>
+                {
+                    new(clientConfiguration.Secret.Sha256())
+                },
+                AllowedScopes = new[] {"api1"},
+                AllowedGrantTypes = new[] {GrantType.ClientCredentials},
+                AccessTokenType = AccessTokenType.Jwt,
+                AccessTokenLifetime = 7200
+            };
+
+            var webHostBuilder = new IdentityServerTestHostBuilder()
+                .AddClients(client)
+                .AddApiResources(new ApiResource("api1", "api1name"))
+                .AddApiScopes(new ApiScope("api1"))
+                .CreateHostBuilder();
+
+            var identityServerProxy = new IdentityServerHostProxy(webHostBuilder);
+
+            var tokenResponse = await identityServerProxy.GetClientAccessTokenAsync(clientConfiguration, "api1");
+
+            var apiWebHostBuilder = WebHost.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                    services.AddSingleton(identityServerProxy.IdentityServer.CreateHandler()))
+                .UseStartup<Startup>();
+
+            var apiServer = new TestServer(apiWebHostBuilder);
+
+            var apiClient = apiServer.CreateClient();
+
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var apiResponse = await apiClient.GetAsync("api/auth");
+
+            Assert.True(apiResponse.IsSuccessStatusCode, "should have been authenticated!");
         }
     }
 }
